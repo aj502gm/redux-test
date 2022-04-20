0faebdf865c8780f9aca3588de46ddc0de5af555
ajgm502 <andres.gonzalez@ext.firstlight.ai>
2022-04-20 13:37:49 -0600

    Router and routes implementation

---
+++ src/Routes/index.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "../components/Home" 
+import Results from "../components/Results" 
+import Details from "../components/Details" 
+
+const RoutesList = () => (
+    <BrowserRouter>
+        <Routes>
+            <Route path = "/" element = {<Home />}/>
+            <Route path = "/results" element = {<Results />}/>
+            <Route path = "/resultsDetails" element = {<Details />}/>
+        </Routes>
+    </BrowserRouter>
+)
+export default RoutesList;
