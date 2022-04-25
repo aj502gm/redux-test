b11e9137d59fbb26e7b30fd4fb745f20fb0d0a6e
ajgm502 <andres.gonzalez@ext.firstlight.ai>
2022-04-25 14:35:15 -0600

    Search Home Screen

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Home() {
+  const [nbaTeam, setNbaTeam] = useState("")
   return (
-    <div className='text-3xl'>Home</div>
-  )
+    <div className="flex h-screen overflow-hidden">
+     <div className="w-3/5 flex justify-center items-center flex-col px-10">
+       <h2 className="text-4xl font-bold font-lato">NBA Team</h2>
+       <input className="bg-special-gray font-lato w-full my-3 h-9 p-1 border focus:outline-none focus:ring-2 focus:ring-gray-500 rounded" value={nbaTeam} onChange={(e) => setNbaTeam(e.target.value)}/>
+       <div className="flex w-full justify-between">
+         <button className="bg-red-500 hover:bg-red-600 text-white font-lato w-full shadow-lg h-9" style={{ width: "48%" }} onClick={() => {}}>
+           Let's go!
+         </button>
+         <button className="bg-red-500 hover:bg-red-600 text-white font-lato w-full shadow-lg h-9" style={{ width: "48%" }} onClick={() => {}}>
+           Clear
+         </button>
+       </div>
+     </div>
+    </div>
+   );
 }
 
 export default Home
